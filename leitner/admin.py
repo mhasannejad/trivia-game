d49d422f3ff7f3e0c06041049cc7a58bfdda34e9
drgeek <mngmnt.hasannejad@gmail.com>
2023-03-01 01:39:10 +0330

    admin panel

--- leitner/admin.py
+++ leitner/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from leitner.models import *
+
+admin.site.register(Daroo)
+admin.site.register(FlashCard)
+admin.site.register(Card)
+admin.site.register(Pharma)
